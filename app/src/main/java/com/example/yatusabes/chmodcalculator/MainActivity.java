9d0f0e175b478c90d4c8263acfe1c06f49cb2773
Margie YNM <margie.ynm@gmail.com>
2018-06-13 15:45:13 -0700

    refactor code

--- app/src/main/java/com/example/yatusabes/chmodcalculator/MainActivity.java
+++ app/src/main/java/com/example/yatusabes/chmodcalculator/MainActivity.java
@@ -28,186 +28,62 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox checkboxOtherWrite;
     private CheckBox checkboxOtherExecute;
 
+    private CompoundButton.OnCheckedChangeListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int[] listenerIds = {
+                R.id.other_execute,
+                R.id.other_read,
+                R.id.other_write,
+                R.id.group_execute,
+                R.id.group_read,
+                R.id.group_write,
+                R.id.user_execute,
+                R.id.user_read,
+                R.id.user_write
+        };
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        creatingUserReadListener();
-        creatingUserWriteListener();
-        creatingUserExecuteListener();
-        creatingGroupReadListener();
-        creatingGroupWriteListener();
-        creatingGroupExecuteListener();
-        creatingOtherReadListener();
-        creatingOtherWriteListener();
-        creatingOtherExecuteListener();
+        listener = new CompoundButton.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
+                int id = checkBox.getId();
+                int val = 0;
+
+                switch (id) {
+                    case R.id.other_execute: val = 1; break;
+                    case R.id.other_read: val = 2; break;
+                    case R.id.other_write: val = 4; break;
+                    case R.id.group_execute: val = 10; break;
+                    case R.id.group_read: val = 20; break;
+                    case R.id.group_write: val = 40; break;
+                    case R.id.user_execute: val = 100; break;
+                    case R.id.user_read: val = 200; break;
+                    case R.id.user_write: val = 400; break;
+                }
+                if (isChecked) {
+                    numPermission += val;
+                } else {
+                    numPermission -= val;
+                }
+
+                showOutput();
+            }
+        };
+        for (int id : listenerIds) {
+            createListener(id);
+        }
 
         showOutput();
 
     }
 
-    private void creatingOtherExecuteListener() {
-        checkboxOtherExecute = (CheckBox) findViewById(R.id.other_execute);
+    private void createListener(int id) {
+        checkboxOtherExecute = (CheckBox) findViewById(id);
         checkboxOtherExecute.setChecked(false);
 
-        checkboxOtherExecute.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 1;
-                } else {
-                    numPermission -= 1;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-    private void creatingOtherWriteListener() {
-        checkboxOtherWrite = (CheckBox) findViewById(R.id.other_write);
-        checkboxOtherWrite.setChecked(false);
-
-        checkboxOtherWrite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 2;
-                } else {
-                    numPermission -= 2;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-    private void creatingOtherReadListener() {
-        checkboxOtherRead = (CheckBox) findViewById(R.id.other_read);
-        checkboxOtherRead.setChecked(false);
-
-        checkboxOtherRead.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 4;
-                } else {
-                    numPermission -= 4;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-    private void creatingGroupExecuteListener() {
-        checkboxGroupExecute = (CheckBox) findViewById(R.id.group_execute);
-        checkboxGroupExecute.setChecked(false);
-
-        checkboxGroupExecute.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 10;
-                } else {
-                    numPermission -= 10;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-    private void creatingGroupWriteListener() {
-        checkboxGroupWrite = (CheckBox) findViewById(R.id.group_write);
-        checkboxGroupWrite.setChecked(false);
-
-        checkboxGroupWrite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 20;
-                } else {
-                    numPermission -= 20;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-    private void creatingGroupReadListener() {
-        checkboxGroupRead = (CheckBox) findViewById(R.id.group_read);
-        checkboxGroupRead.setChecked(false);
-
-        checkboxGroupRead.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 40;
-                } else {
-                    numPermission -= 40;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-    private void creatingUserExecuteListener() {
-        checkboxUserExecute = (CheckBox) findViewById(R.id.user_execute);
-        checkboxUserExecute.setChecked(false);
-
-        checkboxUserExecute.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 100;
-                } else {
-                    numPermission -= 100;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-    private void creatingUserWriteListener() {
-        checkboxUserWrite = (CheckBox) findViewById(R.id.user_write);
-        checkboxUserWrite.setChecked(false);
-
-        checkboxUserWrite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 200;
-                } else {
-                    numPermission -= 200;
-                }
-
-                showOutput();
-            }
-        });
-    }
-
-
-    private void creatingUserReadListener() {
-        checkboxUserRead = (CheckBox) findViewById(R.id.user_read);
-        checkboxUserRead.setChecked(false);
-
-        checkboxUserRead.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            public void onCheckedChanged(CompoundButton checkBox, boolean isChecked) {
-                if (isChecked) {
-                    numPermission += 400;
-                } else {
-                    numPermission -= 400;
-                }
-
-                showOutput();
-            }
-        });
+        checkboxOtherExecute.setOnCheckedChangeListener(listener);
     }
 
     private void showOutput() {
@@ -225,82 +101,6 @@ public class MainActivity extends AppCompatActivity {
         secondAnswerField.setText(outputString);
     }
 
-//    checkBox = (CheckBox)v.findViewById(R.id.approved_checkbox);
-//    checkBox.setChecked(true);
-//
-//    checkBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-//        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//            // update your model (or other business logic) based on isChecked
-//        }
-//    });
-
-//    public void onCheckboxClicked(View view) {
-//        boolean checked = ((CheckBox) view).isChecked();
-//
-//        switch (view.getId()) {
-//            case R.id.user_read:
-//                if (checked)
-//                    numPermission += 400;
-//                else
-//                    numPermission -= 400;
-//                break;
-//            case R.id.user_write:
-//                if (checked)
-//                    numPermission +=200;
-//                else
-//                    numPermission -=200;
-//                Log.d(LOG_TAG, "result: " + numPermission);
-//                break;
-//            case R.id.user_execute:
-//                if (checked)
-//                    numPermission += 100;
-//                else
-//                    numPermission -= 100;
-//                break;
-//            case R.id.group_read:
-//                if (checked)
-//                    numPermission +=40;
-//                else
-//                    numPermission -=40;
-//                Log.d(LOG_TAG, "result: " + numPermission);
-//                break;
-//            case R.id.group_write:
-//                if (checked)
-//                    numPermission += 20;
-//                else
-//                    numPermission -= 20;
-//                break;
-//            case R.id.group_execute:
-//                if (checked)
-//                    numPermission +=10;
-//                else
-//                    numPermission -=10;
-//                Log.d(LOG_TAG, "result: " + numPermission);
-//                break;
-//            case R.id.other_read:
-//                if (checked)
-//                    numPermission +=4;
-//                else
-//                    numPermission -=4;
-//                Log.d(LOG_TAG, "result: " + numPermission);
-//                break;
-//            case R.id.other_write:
-//                if (checked)
-//                    numPermission +=2;
-//                else
-//                    numPermission -=2;
-//                break;
-//            case R.id.other_execute:
-//                if (checked)
-//                    numPermission +=1;
-//                else
-//                    numPermission -=1;
-//                Log.d(LOG_TAG, "result: " + numPermission);
-//                break;
-//
-//        }
-//        showOutput();
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
